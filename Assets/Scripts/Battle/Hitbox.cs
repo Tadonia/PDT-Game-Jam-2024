6557163274393761b6d86e1220b8e9e18f29e3c6
Quang Duong <quanglienduong@gmail.com>
2024-05-31 00:11:58 +1000

    Made hitbox display now move and rotate properly, added player look cursor, added player world attack, more progress into battle initiation

--- Assets/Scripts/Battle/Hitbox.cs
+++ Assets/Scripts/Battle/Hitbox.cs
@@ -14,40 +14,52 @@ public enum AttackType
 public class Hitbox : MonoBehaviour
 {
     [SerializeField]
-    bool active;
+    private bool toggleHitbox;
     [SerializeField, Tooltip("Change how the hitbox behaves.")]
     AttackType attackType;
+    [SerializeField, Tooltip("The allegiance of the hixbox.")]
+    Allegiance allegiance;
     [SerializeField, Tooltip("World: Decides who goes first and player deals starting damage\nBattle: Deals damage during battle")] 
     int damage;
 
     [Header("Debug")]
     [SerializeField]
     bool drawHitbox;
-    [SerializeField, Range(0.0f, 1.0f)]
-    float hitboxOpacity = 0.3f;
+    [SerializeField]
+    Color hitboxColor = new Color(1, 0, 0, 0.3f);
 
-    // Start is called before the first frame update
+    Collider[] colliders;
+
     void Start()
     {
-        if (Application.IsPlaying(gameObject) && !CheckColliders())
+        if (Application.IsPlaying(gameObject) && !FindColliders())
         {
             Debug.LogWarning(gameObject.name + " has no 3D colliders!");
             ToggleColliders();
         }
     }
 
-    private bool CheckColliders()
+    void OnEnable()
     {
-        return GetComponents<Collider>() != null;
+        if (Application.IsPlaying(gameObject) && !FindColliders())
+        {
+            Debug.LogWarning(gameObject.name + " has no 3D colliders!");
+            ToggleColliders();
+        }
+    }
+
+    private bool FindColliders()
+    {
+        return (colliders = GetComponents<Collider>()) != null;
     }
 
     public void SetActive(bool set)
     {
-        active = set;
+        toggleHitbox = set;
         ToggleColliders();
         int count = 0;
 
-        foreach (Collider c in GetComponents<Collider>())
+        foreach (Collider c in colliders)
         {
             if (c.enabled == false) count++;
         }
@@ -58,45 +70,54 @@ public class Hitbox : MonoBehaviour
     /// </summary>
     private void ToggleColliders()
     {
-        foreach (Collider c in GetComponents<Collider>())
+        FindColliders();
+
+        foreach (Collider c in colliders)
         {
-            c.enabled = active;
+            c.enabled = toggleHitbox;
         }
+    }
+
+    public bool GetToggleStatus()
+    {
+        return toggleHitbox;
     }
 
     private void OnDrawGizmosSelected()
     {
-        if (drawHitbox && active)
+        if (drawHitbox && toggleHitbox)
         {
-            Gizmos.color = new(1, 0, 0, hitboxOpacity);
+            Gizmos.color = hitboxColor;
+            Matrix4x4 rotationMatrix = transform.localToWorldMatrix;
+            Gizmos.matrix = rotationMatrix;
 
             foreach (BoxCollider box in GetComponents<BoxCollider>())
             {
-                if (box.enabled) Gizmos.DrawCube(box.center + transform.position, box.size);
+                if (box.enabled) Gizmos.DrawCube(box.center, box.size);
             }
 
             foreach (SphereCollider sphere in GetComponents<SphereCollider>())
             {
-                if (sphere.enabled) Gizmos.DrawSphere(sphere.center + transform.position, sphere.radius);
+                if (sphere.enabled) Gizmos.DrawSphere(sphere.center, sphere.radius);
             }
 
             foreach (MeshCollider mesh in GetComponents<MeshCollider>())
             {
-                if (mesh.enabled) Gizmos.DrawMesh(mesh.sharedMesh, transform.position);
+                if (mesh.enabled) Gizmos.DrawMesh(mesh.sharedMesh);
             }
         }
     }
 
     private void OnValidate()
     {
-        SetActive(active);
+        SetActive(toggleHitbox);
     }
 
     private void OnCollisionStay(Collision collision)
     {
         if (attackType == AttackType.World && collision.gameObject.TryGetComponent<IWorldHittable>(out IWorldHittable hittable))
         {
-            hittable.OnWorldHit();
+            hittable.OnWorldHit(allegiance, damage, gameObject);
         }
 
         if (attackType == AttackType.Battle)
@@ -104,4 +125,5 @@ public class Hitbox : MonoBehaviour
 
         }
     }
+
 }
--- Assets/Scripts/World/IWorldHittable.cs
+++ Assets/Scripts/World/IWorldHittable.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public interface IWorldHittable
 {
-    public void OnWorldHit();
+    /// <summary>
+    /// Calls when a hitbox hits objects with IWorldHittable
+    /// </summary>
+    /// <param name="allegiance">The allegiance of the object that hit the target</param>
+    /// <param name="damage">The damage the target will receive in world or battle</param>
+    /// <param name="hitter">The object that hit the target</param>
+    public void OnWorldHit(Allegiance allegiance, int damage, GameObject hitter);
 }

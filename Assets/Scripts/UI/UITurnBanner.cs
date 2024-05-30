b12f37d5ec87b91a4f5446e3f164880c3afd43e9
Tam\tamduong <tamlienduong03@gmail.com>
2024-05-30 18:11:44 +1000

    Added a turn banner to know whose turn it is

---
+++ Assets/Scripts/UI/UITurnBanner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UITurnBanner : MonoBehaviour
+{
+    [SerializeField] Image background;
+    [SerializeField] TMP_Text turnText;
+
+    public void Start()
+    {
+        TurnManager.Instance.onNextTurn += SetTurn;
+        TurnManager.Instance.onBattleStart += OnBattleStart;
+        background.enabled = false;
+        turnText.enabled = false;
+    }
+
+    private void OnDisable()
+    {
+        TurnManager.Instance.onNextTurn -= SetTurn;
+    }
+
+    public void OnBattleStart()
+    {
+        background.enabled = true;
+        turnText.enabled = true;
+    }
+
+    public void SetTurn(BattleActor actor)
+    {
+        turnText.text = string.Format("{0}'s Turn", actor.name);
+        background.color = actor.allegiance switch
+        {
+            ActorAllegiance.Player => new Color(0f, 0f, 0.75f, 0.5f),
+            ActorAllegiance.Enemy => new Color(0.75f, 0f, 0f, 0.5f),
+            _ => new Color(0f, 0f, 0f, 0.5f),
+        };
+    }
+}
